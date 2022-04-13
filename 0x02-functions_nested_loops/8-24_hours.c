268d42e9cf02f96ae15b161a3194ecff1b8918d5
Olise <you@example.abanum00@gmail.com>
2022-04-12 19:50:42 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,6 @@
  */
 
 void jack_bauer(void)
-	
 {
 
 int a, b, c, d;
@@ -16,15 +15,11 @@ int a, b, c, d;
 
 
 for (a = 48; a <= 50; a++)
-
-	{
-		for (b = 48; b <= 57; b++)
-
+{
+	for (b = 48; b <= 57; b++)
 		{
-
 			for (c = 48; c <= 53; c++)
 			{
-
 				for (d = 48; d <= 57; d++)
 				{
 					if (a >= 50 && b >= 52)
