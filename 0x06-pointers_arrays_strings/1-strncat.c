029003a203237e51f9716881b2d0c9e2d6f31a7a
Olise <you@example.abanum00@gmail.com>
2022-04-24 16:42:58 -0700

    string concatenation with limit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Return: void
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -18,7 +18,7 @@ char *_strcat(char *dest, char *src)
 	{									i++;						
 	}
 	j = 0;
-	while (src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{	
 		dest[i] = src[j];
 		i++;
