f034078c16b36bbd75798d4f6f71c8d259eff4b2
Olise <you@example.abanum00@gmail.com>
2022-05-05 18:30:56 -0700

    Code review task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * Return: Always 0
+ * Return: void
  */
 
 void print_alphabet_x10(void)
@@ -12,22 +12,12 @@ void print_alphabet_x10(void)
 
 	int i, a;
 
-
-
 	for (i = 0; i <= 9; i++)
-
 	{
-
 		for (a = 97; a <= 122; a++)
-
 		{
-
 			_putchar(a);
-
 		}
-
 		_putchar('\n');
-
-	}
-
+	} 
 }
