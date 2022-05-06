1850cdd571f7ca6fca7bee081ba22f781a48e23e
Olise <you@example.abanum00@gmail.com>
2022-05-05 18:54:05 -0700

    Code review task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
-
-
 /**
  * _abs - return abs value
  * @n : number to check
- * Return:0 or 1
+ * Return: int
  */
-
-
 
 int _abs(int n)
 
 {
-
-
-
-	return (n >= 0 ? n : n * -1);
-
-
-
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
 }
