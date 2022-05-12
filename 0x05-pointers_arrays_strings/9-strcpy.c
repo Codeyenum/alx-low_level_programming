8b820e204793267caa9d04a6b18e6e2cad87d7d0
Olise <you@example.abanum00@gmail.com>
2022-05-12 12:40:17 -0700

    code revision

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - update value.
+ * @dest: destination value.
+ * @src: source value
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src);
+
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return(dest);
+}
