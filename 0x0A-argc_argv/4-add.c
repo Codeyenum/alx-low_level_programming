5af81d64ce0bb938ad72882b26fe29e8acd78405
Olise <you@example.abanum00@gmail.com>
2022-05-16 09:33:11 -0700

    code testing

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (1);
 	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]) > '0' && atoi(argv[i]) < '9')
 		{
