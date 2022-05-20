0a4de2bec723b1c537e178dab3f05cc4e8f39932
Olise <you@example.abanum00@gmail.com>
2022-05-20 09:30:13 -0700

    code revision

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	a = malloc(size * sizeof(*a));
-/*	if (a == NULL)
-		return (NULL);*/
+	if (a == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		a[i] = c;
 	return (a);
